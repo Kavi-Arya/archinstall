b9892ded3774f75fee2a0c89a03b5ee3f68abd5d
Kavi Arya <kaviarya047@tutamail.com>
2023-11-16 22:51:19 +0530

    .

--- install.sh
+++ install.sh
@@ -1,1 +1,20 @@
 #!/bin/sh
+
+dirCreating() {
+  mkdir ~/.clones
+  mkdir ~/.config
+  mkdir -p ~/.local/bin
+}
+
+aurInstall() {
+  sudo pacman -Syy
+  sudo pacman -S --needed base-devel
+  sudo pacman -S git
+  git clone https://aur.archlinux.org/yay.git ~/.clones/yay
+  makepkg -si
+  yay --version
+}
+
+pkgsInstall() {
+
+}
